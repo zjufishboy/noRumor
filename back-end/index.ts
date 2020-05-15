fb58b0ec15a0006915627aff549d4645ebc3230e
laichilam <48975583+laichilam@users.noreply.github.com>
2020-05-15 20:58:25 +0800

    Add index.ts of version 1.00

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,6 +2,7 @@ import express =require('express') ;
 import cors=require("cors");
 import bodyParser= require("body-parser");
 import Utility = require("./Utility/utils");
+import { getAllNews, getHotNews, ConsultQuestion, findUsersQuestions } from './Utility/utils';
 //lib模块引入
 const port = 4003;      // 监听端口
 const app = express();  
@@ -15,6 +16,22 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-app.get("/", Utility.apiIndex);
+
+app.get('/api/news', (req, res)=>{
+  res.json(getAllNews(req.body,res)) 
+})
+app.get('/api/hot', (req, res)=>{
+  res.json(getHotNews(req.body,res)) 
+})
+app.post('/api/consult', (req, res)=>{
+  res.json(ConsultQuestion(req.body,res)) 
+})
+app.get('/api/question/me', (req,res)=>{
+  res.json(findUsersQuestions(req.body,res))
+})
+// app.get("/hot", Utility.getHotNews);
+// app.get("/consult", Utility.ConsultQuestion);
+// app.get("/question/me", Utility.findUsersQuestions);
 //listen具体的接口配置
 app.listen(port, () => console.log(`serverA start to listen on port[${port}]`));
+
