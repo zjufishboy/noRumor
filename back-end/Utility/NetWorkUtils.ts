3dd7e8a2a59439119cf956e3daa8055d3e8e4618
laichilam <48975583+laichilam@users.noreply.github.com>
2020-05-16 05:35:55 +0800

    Add file of version 1.1
    reconstructed to a more stable form

--- back-end/Utility/NetWorkUtils.ts
+++ back-end/Utility/NetWorkUtils.ts
@@ -10,114 +10,260 @@ import { IQuestion } from "../types/IQuestion";
  */
 export const request = require('request')
 export const express = require('express')
-export const dao = require('mysql.js')
+export const dao = require('./mysql.js')
 export const timeformat = require('silly-datetime')
 
 
 
-export const getAllNews = (req:INews,res:any)=>{
-    let NewsList
-        try{
-            NewsList = dao.findNewsAll('default')}
-        catch(error){
-            console.log("fatal error")
+// export const getAllNews = (req:INews,res:any)=>{
+//     let NewsList
+//         try{
+//             NewsList = dao.findNewsAll('default')}
+//         catch(error){
+//             console.log("fatal error")
+//         }
+//         finally{
+//             if (NewsList.length==0||typeof(NewsList)=='undefined'){
+//                 return res.send('No data received')
+//             }
+//             else{
+//                 return res.send(NewsList)
+//             }
+//         }
+//     }
+
+
+export const getHotQuestion = (req:any,res:any)=>{
+    const data = req.body
+    let Ls = []
+    let Ms = []
+    let Feedback={
+        status:"default"
+    }
+    if(req==null){
+        Feedback.status="No request"
+        return res.json(Feedback)
+    }
+        Ls = dao.getAllQuestions("concern")
+        if (Ls.length == 0){
+            Feedback.status="No Questions found"
+            return res.json(Feedback)
+            //none question
         }
-        finally{
-            if (NewsList.length==0||typeof(NewsList)=='undefined'){
-                return res.send('No data received')
+        else{
+            for(let iter=0;iter<Ls.length;iter++){
+                let temp = {
+                    // userAvatar:Ls[0].userAvatar
+                    userName:Ls[iter].userName,
+                        //needed?
+                    time:Ls[iter].time,
+                    content:Ls[iter].content,
+                    reply:Ls[iter].reply
+                }
+                Ms.push(temp)
             }
-            else{
-                return res.send(NewsList)
+            if(Ms.length==0){
+                Feedback.status="Push failed"
+                return res.json(Feedback)
             }
+            return res.json(Ms)
         }
     }
-
-
-    export const getHotNews = (req:INews,res:any)=>{
-        let NewsList
-            try{
-                NewsList = dao.findNewsAll('concern')}
-            catch(error){
-                console.log("fatal error")
+    export const getNewQuestion = (req:any,res:any)=>{
+        const data = req.body
+        let Ls = []
+        let Ms = []
+        let Feedback={
+            status:"default"
+        }
+        if(req==null){
+            Feedback.status="No request"
+            return res.json(Feedback)
+        }
+            Ls = dao.getAllQuestions("date")
+            if (Ls.length == 0){
+                Feedback.status="No Questions found"
+                return res.json(Feedback)
+                //none question
             }
-            finally{
-                if (NewsList.length==0||typeof(NewsList)=='undefined'){
-                    return res.send('No data received')
+            else{
+                for(let iter=0;iter<Ls.length;iter++){
+                    let temp = {
+                        // userAvatar:Ls[0].userAvatar
+                        userName:Ls[iter].userName,
+                            //needed?
+                        time:Ls[iter].time,
+                        content:Ls[iter].content,
+                        reply:Ls[iter].reply
+                    }
+                    Ms.push(temp)
                 }
-                else{
-                    return res.send(NewsList)
+                if(Ms.length==0){
+                    Feedback.status="Push failed"
+                    return res.json(Feedback)
                 }
+                return res.json(Ms)
             }
         }
 
-    export const getLatestNews = (req:INews,res:any)=>{
-        let NewsList
-            try{
-                NewsList = dao.findNewsAll('date')}
-            catch(error){
-                console.log("fatal error")
+    export const getAllNews = (req:any,res:any)=>{
+        const data = req.body
+        let Ls = []
+        let Ms = []
+        let Feedback={
+            status:"default"
+        }
+        if(req==null){
+            Feedback.status="No request"
+            return res.json(Feedback)
+        }
+        if(req.body=null){
+            Feedback.status = "No data received"
+            return res.json(Feedback)
+        }
+        if(data.date==null){
+            Feedback.status = "Date missing"
+            return res.json(Feedback)
+        }else{
+            Ls = dao.getAllNews("date")
+            if (Ls.length == 0){
+                Feedback.status="No News found"
+                return res.json(Feedback)
+                //none question
             }
-            finally{
-                if (NewsList.length==0||typeof(NewsList)=='undefined'){
-                    return res.send('No data received')
+            else{
+                for(let iter=0;iter<Ls.length;iter++){
+                    let temp = {
+                        // userAvatar:Ls[0].userAvatar
+                        title:Ls[iter].content,
+                        //needed?
+                        pic:Ls[iter].pic,
+                        //数据库内无pic属性，是保存成文件？暂时这么写
+                        content:Ls[iter].content,
+                        //不获取发布者吗？
+                        status:Ls[iter].truth
+                    }
+                    Ms.push(temp)
                 }
-                else{
-                    return res.send(NewsList)
+                if(Ms.length==0){
+                    Feedback.status="Push failed"
+                    return res.json(Feedback)
                 }
+                return res.json(Ms)
+            }
             }
         }
     
 
-    export const findUsersQuestions = (req:IQuestion,res:any)=>{
-        
-        let User = req.userName
-        let QuestionList =[]
-        let MiddleList: IQuestion[] =[]
-        if(User == null){
-            return res.send('User doesnt exist!')
+    export const findUsersQuestions = (req:any,res:any)=>{
+        const data = req.body
+        let Ls = [ ]
+        //QuestionList
+        let Ms = [ ]
+        //tempList
+        let Feedback={
+            status:"default"
+        }
+        if(req==null){
+            Feedback.status="No request"
+            return res.json(Feedback)
+        }
+        if(req.body=null){
+            Feedback.status = "No data received"
+            return res.json(Feedback)
+        }
+        if(data.userName == null){
+            Feedback.status="Empty user"
+            return res.json(Feedback)
             //cant find user
         }
         else{
-            QuestionList = dao.findQuestionsbyUid(User)
-            if (QuestionList.length == 0){
-                return res.send('No questions found!')
+            Ls = dao.findQuestionsbyUid(data.userName)
+            if (Ls.length == 0){
+                Feedback.status="No Question found"
+                return res.json(Feedback)
                 //none question
             }
             else{
-                let iter = 0
-                for(iter=0;iter<QuestionList.length;iter++){
-                    MiddleList[iter].userName = QuestionList[iter].userName
-                    MiddleList[iter].time = QuestionList[iter].time
-                    MiddleList[iter].content = QuestionList[iter].content
+                for(let iter=0;iter<Ls.length;iter++){
+                    let temp = {
+                        // userAvatar:Ls[0].userAvatar
+                        userName:Ls[iter].userName,
+                        //needed?
+                        time:Ls[iter].time,
+                        content:Ls[iter].content,
+                        reply:Ls[iter].reply
+                    }
+                    Ms.push(temp)
                 }
-                return res.send(MiddleList)
+                if(Ms.length==0){
+                    Feedback.status="Push failed"
+                    return res.json(Feedback)
+                }
+                return res.json(Ms)
             }
         }
     }
 
-    // question -> boolean
-    export const ConsultQuestion = (req:IQuestion,res:any)=>{
-        let CreateDone:boolean
-        if (req == null){
-            return res.send('Empty Question')
-            // null request
-        }
-        else{
-            let n_content = req.content
-            let n_time = req.time
-            let n_answer = "not answered"
-            let n_uid_o = req.userName
-            let n_uid_p = null
-            let n_pid //
-            CreateDone = dao.createNews(n_answer,n_content,n_pid,n_time,n_uid_o,n_uid_p)
-            if (CreateDone==true){
-                return res.send('Question Created')
-            }
-            else{
-                return res.send('Creating failed')
-                //creating failed
-            }
+    export const ConsultQuestion = (req:any,res:any)=>{
             
-        }
+        let CreateSuccess = false
+        let NewQue = {
+                n_userAvatar: "",
+                n_userName: "",
+                n_time: "",
+                n_content:"",
+                n_reply:{ }
+            }
+            let Feedback ={
+                status:"default"
+            }
+            //反馈信息
+            if(req==null){
+                Feedback.status="no request"
+                return res.json(Feedback)
+            }
+            if(req.body=null){
+                Feedback.status = "No data received"
+                return res.json(Feedback)
+            }
+            const data = req.body
+            //请求中拿出body数据
+
+            if(!data.userAvatar||!data.userName||!data.time||!data.content){
+                Feedback.status = "attributes missing"
+                return res.json(Feedback)
+                //某一项缺失
+            }
+            NewQue.n_userAvatar = data.userAvatar
+            NewQue.n_userName = data.userName
+            NewQue.n_time = data.time
+            NewQue.n_content = data.content
+
+            if(!data.reply){
+                //未回复
+                let rep = {
+                    r_userAvatar:"",
+                    r_userName:"",
+                    r_content:"haven't got reply"
+                }
+                // CreateSuccess = dao.createNews(rep,NewQue.n_content,n.pid,NewQue.n_time,n.uid_o,n.uid_p)
+                CreateSuccess = dao.createNews(rep,NewQue.n_content,NewQue.n_time,NewQue.n_userName)
+                // 
+            }else{
+                let rep = {
+                    r_userAvatar:data.reply.userAvatar,
+                    r_userName:data.reply.userName,
+                    r_content:data.reply.content
+                }
+                CreateSuccess = dao.createNews(rep,NewQue.n_content,NewQue.n_time,NewQue.n_userName)
+
+            }
+            if (!CreateSuccess){
+                Feedback.status = "Create Failed"
+                return res.json(Feedback)
+            }
+            Feedback.status = "Create Success"
+            return res.json(Feedback)
     }
 
