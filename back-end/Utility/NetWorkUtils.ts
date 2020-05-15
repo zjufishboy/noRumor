90f7507548979c7cf01b0619551e55d68b11c5a3
laichilam <48975583+laichilam@users.noreply.github.com>
2020-05-15 20:17:29 +0800

    Add file of version 1.01

--- back-end/Utility/NetWorkUtils.ts
+++ back-end/Utility/NetWorkUtils.ts
@@ -1,4 +1,4 @@
-
+import INews, { } from "../types/INews";
 import { IQuestion } from "../types/IQuestion";
 /**
  * 这里是接口相关的函数
@@ -12,64 +12,94 @@ export const request = require('request')
 export const express = require('express')
 export const dao = require('mysql.js')
 export const timeformat = require('silly-datetime')
-export const formidable = require("formidable");
-
-const a = express();
 
 
-export const getAllNews = (req:{},res:any)=>{
-        let NewsList = dao.findNewsAll('default')
-        if (NewsList.length==0){
-            return res.send('-1')
+
+export const getAllNews = (req:INews,res:any)=>{
+    let NewsList
+        try{
+            NewsList = dao.findNewsAll('default')}
+        catch(error){
+            console.log("fatal error")
         }
-        else{
-            return res.send(NewsList)
-        }
-    }
-
-
-    export const getHotNews = (req:any,res:any)=>{
-        let HotList = dao.findNewsAll('cocern')
-        if (HotList.length==0){
-            return res.send('-1')
-        }
-        else{
-            return res.send(HotList)   
+        finally{
+            if (NewsList.length==0||typeof(NewsList)=='undefined'){
+                return res.send('No data received')
+            }
+            else{
+                return res.send(NewsList)
+            }
         }
     }
-    export const getLatestNews = (req:any,res:any)=>{
-        let NewsList = dao.findNewsAll('date')
-        if (NewsList.length == 0){
-            return res.send('-1')
+
+
+    export const getHotNews = (req:INews,res:any)=>{
+        let NewsList
+            try{
+                NewsList = dao.findNewsAll('concern')}
+            catch(error){
+                console.log("fatal error")
+            }
+            finally{
+                if (NewsList.length==0||typeof(NewsList)=='undefined'){
+                    return res.send('No data received')
+                }
+                else{
+                    return res.send(NewsList)
+                }
+            }
         }
-            return res.send(NewsList)   
+
+    export const getLatestNews = (req:INews,res:any)=>{
+        let NewsList
+            try{
+                NewsList = dao.findNewsAll('date')}
+            catch(error){
+                console.log("fatal error")
+            }
+            finally{
+                if (NewsList.length==0||typeof(NewsList)=='undefined'){
+                    return res.send('No data received')
+                }
+                else{
+                    return res.send(NewsList)
+                }
+            }
         }
     
 
-    export const findUsersQuestions = (req:any,res:any)=>{
+    export const findUsersQuestions = (req:IQuestion,res:any)=>{
         
-        let User = req.body.userName
+        let User = req.userName
+        let QuestionList =[]
+        let MiddleList: IQuestion[] =[]
         if(User == null){
-            return res.send('-1')
+            return res.send('User doesnt exist!')
             //cant find user
         }
         else{
-            let QuestionList = dao.findQuestionsbyUid(User)
-            if (QuestionList = null){
-                return res.send('-2')
+            QuestionList = dao.findQuestionsbyUid(User)
+            if (QuestionList.length == 0){
+                return res.send('No questions found!')
                 //none question
             }
             else{
-                return res.send(QuestionList)
+                let iter = 0
+                for(iter=0;iter<QuestionList.length;iter++){
+                    MiddleList[iter].userName = QuestionList[iter].userName
+                    MiddleList[iter].time = QuestionList[iter].time
+                    MiddleList[iter].content = QuestionList[iter].content
+                }
+                return res.send(MiddleList)
             }
         }
     }
 
     // question -> boolean
-    export const ConsultQuestion = (req:any,res:any)=>{
-        let NewQ = new dao()
+    export const ConsultQuestion = (req:IQuestion,res:any)=>{
+        let CreateDone:boolean
         if (req == null){
-            return res.send('-1')
+            return res.send('Empty Question')
             // null request
         }
         else{
@@ -79,12 +109,12 @@ export const getAllNews = (req:{},res:any)=>{
             let n_uid_o = req.userName
             let n_uid_p = null
             let n_pid //
-            let CreateDone = NewQ.createNews(n_answer,n_content,n_pid,n_time,n_uid_o,n_uid_p)
+            CreateDone = dao.createNews(n_answer,n_content,n_pid,n_time,n_uid_o,n_uid_p)
             if (CreateDone==true){
-                return res.send('1')
+                return res.send('Question Created')
             }
             else{
-                return res.send('-2')
+                return res.send('Creating failed')
                 //creating failed
             }
             
