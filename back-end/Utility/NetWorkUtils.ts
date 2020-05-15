dc2582c81114f8fb3b7d3c7a8ee6ce127b0b2ff9
laichilam <48975583+laichilam@users.noreply.github.com>
2020-05-15 18:58:48 +0800

    Add file of version 1
    haven't debugged

--- back-end/Utility/NetWorkUtils.ts
+++ back-end/Utility/NetWorkUtils.ts
@@ -30,7 +30,7 @@ export const getAllNews = (req:{},res:any)=>{
 
     export const getHotNews = (req:any,res:any)=>{
         let HotList = dao.findNewsAll('cocern')
-        if (HotList==null){
+        if (HotList.length==0){
             return res.send('-1')
         }
         else{
@@ -91,51 +91,3 @@ export const getAllNews = (req:{},res:any)=>{
         }
     }
 
-/*
-    example:
-                userAvatar: 'http://q2.qlogo.cn/headimg_dl?dst_uin=1647075274&spec=100',
-                userName: '@cname',
-                time: '@DATETIME("yyyy-MM-dd")',
-                content: '保定又有新病例了吗',
-                reply: {
-                    userAvatar: 'http://q2.qlogo.cn/headimg_dl?dst_uin=1647075274&spec=100',
-                    userName: '全民较真',
-                    content: '你好，《国务院客户端》截止4月18日18时部分地区疫情风险情况，将北京市朝阳区列为高风险地区。',
-                },
-*/ 
-
-/*               
-                title: '组合用羟氯喹阿奇霉素和硫酸锌能有效治疗新冠肺炎 ',
-                picture: 'https://p.qpic.cn/jiaozhen/0/fedf2805ba54f963349bd51b4c716d7f/0?imageView2/2/w/150/h/90',
-                time: '@DATETIME("yyyy-MM-dd")',
-                status: 'false',
-                 */
-// export const createNews =(req:{},res:JSON)=>{
-//     dao.createNews(req,res)
-//     res.send
-// } 
-// export const deleteNews =(req:{},res:JSON)=>{
-//     dao.deleteNews(req,res)
-// } 
-// export const findNewsByUser =(req:{},res:JSON)=>{
-//     dao.findNewsByUser(req,res)
-// } 
-// export const findNewsByDate =(req:{},res:JSON)=>{
-//     dao.findNewsByDate(req,res)
-// } 
-// export const findQuestionsByUser=()=>{}
-// export const findQuestionsByDate=()=>{}
-
-// export const getAllNews=()=>{
-//     dao.getAllNews()
-// }
-// export const getAllQuestions=()=>{}
-// export const 
-/**
- * 关于接口的使用，可以看到上面这个箭头函数的例子里，
- * 参数后面有一个对象来表示这个对象具体的内容，
- * 因为js是弱类型语言，而ts是强类型语言，
- * 我们在这种调用的时候需要写入接口来表示数据格式。
- * 具体的数据接口应该写到types文件夹下，
- * 并且保证前端和后端使用同一套接口
- */
