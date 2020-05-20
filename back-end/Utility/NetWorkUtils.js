f2877b5c9c93479e6d20199fc1f821e7305b7680
laichilam <48975583+laichilam@users.noreply.github.com>
2020-05-20 14:53:00 +0800

    Add files via upload

--- back-end/Utility/NetWorkUtils.js
+++ back-end/Utility/NetWorkUtils.js
@@ -1,366 +1,353 @@
-/**
- * 这里是接口相关的函数
- * 主要函数形式请写成：
- * export const XXX = (req,res)=>{}
- * 的形式
- * 这里的req是前端发过来的请求，res主要只用res.send(jsonObject)来处理
- * 主要是业务层的工作
- */
-// const Dao = require('mysql')
+"use strict";
+exports.__esModule = true;
+exports.getHotNews = exports.getHotSearch = exports.ConsultQuestion = exports.findUsersQuestions = exports.getAllNews = exports.getNewQuestion = exports.getHotQuestion = exports.dao = void 0;
+exports.dao = require('./mysql.js');
 /*
 获取热搜问题
 req: req
 res: IQuestion[] / Feedback
 */
-exports.getHotQuestion = (req,res)=>{
-    const data = req.body
-    let Ls = []
-    let Ms = []
-    let Feedback={
-        status:"default"
+exports.getHotQuestion = function (req, res) {
+    var data = req.body;
+    var Ls = [];
+    var Ms = [];
+    var Feedback = {
+        status: "default"
+    };
+    if (req == null) {
+        Feedback.status = "No Request Received";
+        return res.json(Feedback);
     }
-
-    if(req==null){
-        Feedback.status="No Request Received"
-        return res.json(Feedback)
-    }
-
-    Ls = dao.getAllQuestions("concern")
-    if (Ls.length == 0){
-        Feedback.status="No Questions Found"
-        return res.json(Feedback)
+    Ls = exports.dao.getAllQuestions("concern");
+    if (Ls.length == 0) {
+        Feedback.status = "No Questions Found";
+        return res.json(Feedback);
         //none question
     }
-
-    else{
-        for(let iter=0;iter<Ls.length;iter++){
-            let temp = {
+    else {
+        for (var iter = 0; iter < Ls.length; iter++) {
+            var temp = {
                 // userAvatar:Ls[0].userAvatar
-                userName:Ls[iter].userName,
-                    //needed?
-                time:Ls[iter].time,
-                content:Ls[iter].content,
-                reply:Ls[iter].reply}
-                
-                Ms.push(temp)}
-        if(Ms.length==0){
-            Feedback.status="Push Filed"
-            return res.json(Feedback)}
-        
-        return res.json(Ms)
+                userName: Ls[iter].userName,
+                //needed?
+                time: Ls[iter].time,
+                content: Ls[iter].content,
+                reply: Ls[iter].reply
+            };
+            Ms.push(temp);
         }
+        if (Ms.length == 0) {
+            Feedback.status = "Push Filed";
+            return res.json(Feedback);
+        }
+        return res.json(Ms);
     }
-
+};
 /*
 获取最新问题
 req: req.body:date
 res: IQuestion[] / Feedback
 */
-exports.getNewQuestion = (req,res)=>{
-    const data = req.body
-    let Ls = []
-    let Ms = []
-    let Feedback={
-        status:"default"}
-    if(req==null){
-        Feedback.status="No Request"
-        return res.json(Feedback)
+exports.getNewQuestion = function (req, res) {
+    var data = req.body;
+    var Ls = [];
+    var Ms = [];
+    var Feedback = {
+        status: "default"
+    };
+    if (req == null) {
+        Feedback.status = "No Request";
+        return res.json(Feedback);
     }
-    dao.getAllQuestions.then(results=>{
-        if(results.length!=0){
-            Ls = dao.getAllQuestions("date")
-        }else{
-            Feedback.status="No Questions Found"
-            return res.json(Feedback)
+    exports.dao.getAllNews('date').then(function (results) {
+        Ls = results;
+    }, function (err) {
+        Feedback.status = "Getting Data Failed";
+        return res.json(Feedback);
+    });
+    if (Ls.length == 0) {
+        Feedback.status = "No Questions Found";
+        return res.json(Feedback);
+    }
+    //none question}
+    else {
+        for (var iter = 0; iter < Ls.length; iter++) {
+            var temp = {
+                // userAvatar:Ls[0].userAvatar
+                userName: Ls[iter].userName,
+                //needed?
+                time: Ls[iter].time,
+                content: Ls[iter].content,
+                reply: Ls[iter].reply
+            };
+            Ms.push(temp);
         }
-    })
-        for(let iter=0;iter<Ls.length;iter++){
-            let temp = {
-            // userAvatar:Ls[0].userAvatar
-            userName:Ls[iter].userName,
-            //needed?
-            time:Ls[iter].time,
-            content:Ls[iter].content,
-            reply:Ls[iter].reply}
-            
-            Ms.push(temp)}
-        if(Ms.length==0){
-            Feedback.status="Push Failed"
-            return res.json(Feedback)}
-        return res.json(Ms)
+        if (Ms.length == 0) {
+            Feedback.status = "Push Failed";
+            return res.json(Feedback);
         }
+        return res.json(Ms);
+    }
+};
 /*
 获取最新谣言内容
 req: req.body:date
 res: INews[] / Feedback
 */
-exports.getAllNews = (req,res)=>{
-    const data = req.body
-    let Ls = []
-    let Ms = []
-    let Feedback={
-        status:"default"}
-    if(req==null){
-        Feedback.status="No Request"
-        return res.json(Feedback)
+exports.getAllNews = function (req, res) {
+    var data = req.body;
+    var Ls = [];
+    var Ms = [];
+    var Feedback = {
+        status: "default"
+    };
+    if (req == null) {
+        Feedback.status = "No Request";
+        return res.json(Feedback);
     }
-    if(req.body=null){
-        Feedback.status = "No Data Received"
-        return res.json(Feedback)
+    if (req.body = null) {
+        Feedback.status = "No Data Received";
+        return res.json(Feedback);
     }
-    if(data.date==null){
-        Feedback.status = "Date Missing"
-        return res.json(Feedback)
-    }else{
-        dao.getAllNews('date').then((results)=>{
-            if(results.length!=0){
-                Ls = results
-            }else{
-                Feedback.status="No News Found"
-                return res.json(Feedback)
-                //none question}
+    if (data.date == null) {
+        Feedback.status = "Date Missing";
+        return res.json(Feedback);
+    }
+    else {
+        exports.dao.getAllNews('date').then(function (results) {
+            Ls = results;
+        }, function (err) {
+            Feedback.status = "Getting Data Failed";
+            return res.json(Feedback);
+        });
+        if (Ls.length == 0) {
+            Feedback.status = "No News Found";
+            return res.json(Feedback);
+        }
+        //none question}
+        else {
+            for (var iter = 0; iter < Ls.length; iter++) {
+                var temp = {
+                    // userAvatar:Ls[0].userAvatar
+                    title: Ls[iter].content,
+                    //needed?
+                    pic: Ls[iter].pic,
+                    //数据库内无pic属性，是保存成文件？暂时这么写
+                    content: Ls[iter].content,
+                    //不获取发布者吗？
+                    status: Ls[iter].truth
+                };
+                Ms.push(temp);
             }
-        })
-        if (Ls.length == 0){
-            Feedback.status="Copy Failed"
-            return res.json(Feedback)}
-            //none question}
-        else{
-            for(let iter=0;iter<Ls.length;iter++){
-                let temp = {
-                // userAvatar:Ls[0].userAvatar
-                title:Ls[iter].content,
-                //needed?
-                pic:Ls[iter].pic,
-                //数据库内无pic属性，是保存成文件？暂时这么写
-                content:Ls[iter].content,
-                //不获取发布者吗？
-                status:Ls[iter].truth}
-                Ms.push(temp)
+            if (Ms.length == 0) {
+                Feedback.status = "Push Failed";
+                return res.json(Feedback);
             }
-            if(Ms.length==0){
-                Feedback.status="Push Failed"
-                return res.json(Feedback)
-            }
-            return res.json(Ms)
-            }
+            return res.json(Ms);
         }
     }
-    
+};
 /*
 获取最新谣言内容
 req: req.body:userName
 res: IQuestion[] / Feedback
 */
-exports.findUsersQuestions = (req,res)=>{
-    const data = req.body
-    let Ls = [ ]
+exports.findUsersQuestions = function (req, res) {
+    var data = req.body;
+    var Ls = [];
     //QuestionList
-    let Ms = [ ]
+    var Ms = [];
     //tempList
-    let Feedback={
-        status:"default"}
-    if(req==null){
-        Feedback.status="No Request"
-        return res.json(Feedback)}
-    if(req.body=null){
-        Feedback.status = "No Data Received"
-        return res.json(Feedback)}
-    if(data.userName == null){
-        Feedback.status="Empty User"
-        return res.json(Feedback)}
-        //cant find user }
-    else{
-        Ls = dao.findQuestionsbyUid(data.userName).then(results=>{
-            if(results.length!=0){
-                Ls = results
-            }else{
-                Feedback.status="rejected"
-                return res.json(Feedback)
-            }
-        })
-        if (Ls.length == 0){
-            Feedback.status="No Question Found"
-            return res.json(Feedback)}
-            //none question
-        else{
-            for(let iter=0;iter<Ls.length;iter++){
-                let temp = {
+    var Feedback = {
+        status: "default"
+    };
+    if (req == null) {
+        Feedback.status = "No Request";
+        return res.json(Feedback);
+    }
+    if (data.Username == null) {
+        Feedback.status = "Empty User";
+        return res.json(Feedback);
+    }
+    //cant find user }
+    else {
+        exports.dao.findQuestionsbyUid(data.userName).then(function (results) {
+            Ls = results;
+        }, function (err) {
+            Feedback.status = "Getting Failed";
+            return res.json(Feedback);
+        });
+        if (Ls.length == 0) {
+            Feedback.status = "No Question Found";
+            return res.json(Feedback);
+        }
+        //none question
+        else {
+            for (var iter = 0; iter < Ls.length; iter++) {
+                var temp = {
                     // userAvatar:Ls[0].userAvatar
-                    userName:Ls[iter].userName,
+                    userName: Ls[iter].userName,
                     //needed?
-                    time:Ls[iter].time,
-                    content:Ls[iter].content,
-                    reply:Ls[iter].reply}
-                Ms.push(temp)
-                }
-            if(Ms.length==0){
-                Feedback.status="Push Failed"
-                return res.json(Feedback)}
-                
-                return res.json(Ms)
+                    time: Ls[iter].time,
+                    content: Ls[iter].content,
+                    reply: Ls[iter].reply
+                };
+                Ms.push(temp);
             }
+            if (Ms.length == 0) {
+                Feedback.status = "Push Failed";
+                return res.json(Feedback);
+            }
+            return res.json(Ms);
         }
     }
+};
 /*
 提交问题
 req: req.body:IQuetstion
 res: (或者可以返回创建后的id)/Feedback
 */
-exports.ConsultQuestion = (req,res)=>{    
-    let CreateSuccess = false
-    let NewQue = {
+exports.ConsultQuestion = function (req, res) {
+    var CreateSuccess = false;
+    var NewQue = {
         n_userAvatar: "",
         n_userName: "",
         n_time: "",
-        n_content:"",
-        n_reply:{ } }
-    let Feedback ={
-        status:"default"}
-            //反馈信息
-    if(req==null){
-        Feedback.status="No Request"
-        return res.json(Feedback)}
-    
-    if(req.body=null){
-    Feedback.status = "No Data Received"
-    return res.json(Feedback)}
-    
-    const data = req.body
-        //请求中拿出body数据
-
-    if(!data.userAvatar||!data.userName||!data.time||!data.content){
-        Feedback.status = "Attributes Missing"
-        return res.json(Feedback)}
-        //某一项缺失}
-        
-    NewQue.n_userAvatar = data.userAvatar
-    NewQue.n_userName = data.userName
-    NewQue.n_time = data.time
-    NewQue.n_content = data.content
-        
-    if(!data.reply){
-             //未回复
-        let rep = {
-        r_userAvatar:"",
-        r_userName:"",
-        r_content:"Haven't Got Reply"}
-        // CreateSuccess = dao.createNews(rep,NewQue.n_content,n.pid,NewQue.n_time,n.uid_o,n.uid_p)
-        dao.createNews(rep,NewQue.n_content,NewQue.n_time,NewQue.n_userName).then((results)=>{
-            if(results.length!=0){
-                Feedback.status = "Create Success"
-                return res.json(Feedback)
-                }else{
-                    Feedback.status = "Create Failed"
-                    return res.json(Feedback)
-                }
-            })    
-     }
-    else{
-        let rep = {
-        r_userAvatar:data.reply.userAvatar,
-        r_userName:data.reply.userName,
-        r_content:data.reply.content}
-                
-        dao.createNews(rep,NewQue.n_content,NewQue.n_time,NewQue.n_userName).then((results)=>{
-            if(results.length!=0){
-                Feedback.status = "Create Success"
-                return res.json(Feedback)
-                }else{
-                    Feedback.status = "Create Failed"
-                    return res.json(Feedback)
-                }
-            }) 
-        }
-        
-    Feedback.status = "error"
-    return res.json(Feedback)
+        n_content: "",
+        n_reply: {}
+    };
+    var Feedback = {
+        status: "default"
+    };
+    //反馈信息
+    if (req == null) {
+        Feedback.status = "No Request";
+        return res.json(Feedback);
     }
-
+    if (req.body = null) {
+        Feedback.status = "No Data Received";
+        return res.json(Feedback);
+    }
+    var data = req.body;
+    //请求中拿出body数据
+    if (!data.userAvatar || !data.userName || !data.time || !data.content) {
+        Feedback.status = "Attributes Missing";
+        return res.json(Feedback);
+    }
+    //某一项缺失}
+    NewQue.n_userAvatar = data.userAvatar;
+    NewQue.n_userName = data.userName;
+    NewQue.n_time = data.time;
+    NewQue.n_content = data.content;
+    if (!data.reply) {
+        //未回复
+        var rep = {
+            r_userAvatar: "",
+            r_userName: "",
+            r_content: "Haven't Got Reply"
+        };
+        // CreateSuccess = dao.createNews(rep,NewQue.n_content,n.pid,NewQue.n_time,n.uid_o,n.uid_p)
+        exports.dao.createNews(rep, NewQue.n_content, NewQue.n_time, NewQue.n_userName).then(function (results) {
+            Feedback.status = "Create Done";
+            return res.json(Feedback);
+        }), function (err) {
+            Feedback.status = "Create Failed";
+            return res.json(Feedback);
+        };
+    }
+    else {
+        var rep = {
+            r_userAvatar: data.reply.userAvatar,
+            r_userName: data.reply.userName,
+            r_content: data.reply.content
+        };
+        exports.dao.createNews(rep, NewQue.n_content, NewQue.n_time, NewQue.n_userName).then(function (results) {
+            Feedback.status = "Create Done";
+            return res.json(Feedback);
+        }, function (err) {
+            Feedback.status = "Create Failed";
+            return res.json(Feedback);
+        });
+    }
+};
 /*
 获取热搜
 req: req
 res: {content, id}/Feedback
 */
-exports.getHotSearch = (req,res)=>{
-    let Ms = []
-    let Fs = []
-    let Feedback ={
-        status:"default"}
-            //反馈信息
-    if(req==null){
-        Feedback.status="No Request"
-        return res.json(Feedback)}
-    
-    if(req.body=null){
-    Feedback.status = "No Data Received"
-    return res.json(Feedback)}
-    
-    Ms = dao.getAllNews('cocern').then(results=>{
-        if(results.length!=0){
-            Ms=results
-        }else{
-            Feedback.status="reach to dao Failed"
-            return res.json(Feedback)
+exports.getHotSearch = function (req, res) {
+    var Ms = [];
+    var Fs = [];
+    var Feedback = {
+        status: "default"
+    };
+    //反馈信息
+    if (req == null) {
+        Feedback.status = "No Request";
+        return res.json(Feedback);
+    }
+    exports.dao.getAllNews('cocern').then(function (results) {
+        Ms = results;
+    }, function (err) {
+        Feedback.status = "Getting Data Failed";
+        return res.json(Feedback);
+    });
+    for (var iter = 0; iter < Ms.length; iter++) {
+        if (Ms[iter] == null || Ms[iter].title == null || Ms[iter].id == null) {
+            Feedback.status = "Getting Data Failed";
+            return res.json(Feedback);
         }
-    })
-    for(let iter = 0; iter<Ms.length; iter++){
-        if(Ms[iter]==null||Ms[iter].title==null||Ms[iter].id==null){
-            Feedback.status="Getting Data Failed"
-            return res.json(Feedback)
-        }
-        let temp = {
+        var temp = {
             title: Ms[iter].title,
             id: Ms[iter].id
-        }
-        Fs.push(temp)
+        };
+        Fs.push(temp);
     }
-    if(Fs.length==0){
-        Feedback.status="Empty Fs"
-        return res.json(Feedback)
+    if (Fs.length == 0) {
+        Feedback.status = "Empty Fs";
+        return res.json(Feedback);
     }
-    return res.json(Fs)
-}
+    return res.json(Fs);
+};
 /*
 获取hotsearch关键词
 req: req
 res: {hotsearchword}/Feedback
 */
-exports.getHotNews = (req,res)=>{
-    let Ms = []
-    let Fs = []
-    let Feedback ={
-        status:"default"}
-            //反馈信息
-    if(req==null){
-        Feedback.status="No Request"
-        return res.json(Feedback)}
-    
-    if(req.body=null){
-    Feedback.status = "No Data Received"
-    return res.json(Feedback)}
-    
-    Ms = dao.getAllNews('cocern').then(results=>{
-        if(results.length!=0){
-            Ms=results
-        }else{
-            Feedback.status="reach to dao Failed"
-            return res.json(Feedback)
-        }
-    })
-    for(let iter = 0; iter<Ms.length; iter++){
-        if(Ms[iter]==null||Ms[iter].title==null||Ms[iter].id==null){
-            Feedback.status="Getting Data Failed"
-            return res.json(Feedback)
-        }
-        let temp = {
-            title: Ms[iter].title,
-        }
-        Fs.push(temp)
+exports.getHotNews = function (req, res) {
+    var Ms = [];
+    var Fs = [];
+    var Feedback = {
+        status: "default"
+    };
+    //反馈信息
+    if (req == null) {
+        Feedback.status = "No Request";
+        return res.json(Feedback);
     }
-    if(Fs.length==0){
-        Feedback.status="Empty Fs"
-        return res.json(Feedback)
+    if (req.body = null) {
+        Feedback.status = "No Data Received";
+        return res.json(Feedback);
     }
-    return res.json(Fs)
-}
+    exports.dao.getAllNews('cocern').then(function (results) {
+        Ms = results;
+    }, function (err) {
+        Feedback.status = "Getting Data Failed";
+        return res.json(Feedback);
+    });
+    for (var iter = 0; iter < Ms.length; iter++) {
+        if (Ms[iter].title == null || Ms[iter].id == null) {
+            Feedback.status = "Getting Data Failed";
+            return res.json(Feedback);
+        }
+        var temp = {
+            title: Ms[iter].title
+        };
+        Fs.push(temp);
+    }
+    if (Fs.length == 0) {
+        Feedback.status = "Empty Fs";
+        return res.json(Feedback);
+    }
+    return res.json(Fs);
+};
---
+++ back-end/Utility/index.js
@@ -0,0 +1,29 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var ut = require("./Utility/NetWorkUtils");
+//lib模块引入
+var port = 4003; // 监听端口
+var app = express();
+//config配置
+//utils工具函数
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+//listen具体的接口配置
+console.log("listeing");
+app.listen(port, function () { return console.log("serverA start to listen on port[" + port + "]"); });
+app.get('/api/news', ut.getAllNews);
+app.get('/api/hot', ut.getHotSearch);
+app.get('/api/hotSearchList', ut.getHotSearch);
+// app.get('/api/notice.....) 公告系统
+// app.get('/api/searchResult',)
+//log and register system
+app.post('/api/consult', ut.ConsultQuestion);
+app.get('/api/question/hot', ut.getHotQuestion);
+app.get('/api/question/new', ut.getNewQuestion);
+app.get('/api/question/me/:Username', ut.findUsersQuestions);
