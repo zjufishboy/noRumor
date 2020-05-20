544faeadd3bead5b5d5e677158d8d9ccfc298c51
laichilam <48975583+laichilam@users.noreply.github.com>
2020-05-20 18:03:04 +0800

    Add files via upload

--- back-end/index.js
+++ back-end/index.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const ut = require('./Utility/NetWorkUtilsNew')
-const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('./'));
-app.use(express.json({}));
-
-app.get('/api/news',ut.getAllNews)
-// getHotSearch返回[{title, id},.....]
-app.get('/api/hot',ut.getHotSearch)
-// getHotSearchList返回[{title}]
-app.get('/api/hotSearchList',ut.getHotSearchList)
-// app.get('/api/notice.....) 公告系统 待整合
-// app.get('/api/searchResult',) 待整合
-//log and register system 待整合
-app.post('/api/consult', ut.ConsultQuestion)
-app.get('/api/question/hot', ut.getHotQuestion)
-app.get('/api/question/new', ut.getNewQuestion)
-app.get('/api/question/me/:username', ut.findUsersQuestions)
+const express = require("express");
+const bodyParser = require("body-parser");
+const ut = require("./Utility/NetWorkUtils.js");
+//lib模块引入
+var port = 4003; // 监听端口
+const app = express()
+//config配置
+//utils工具函数
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// app.use(express.static('./'))
+app.use(bodyParser.json());
+//listen具体的接口配置
+console.log("listeing");
+app.listen(port, function () { return console.log("serverA start to listen on port[" + port + "]"); });
+app.get('/api/news',ut.getAllNew);
+app.get('/api/hot', ut.getHotSearch);
+app.get('/api/hotSearchList', ut.getHotSearch);
+// app.get('/api/notice.....) 公告系统
+// app.get('/api/searchResult',)
+//log and register system
+// app.post('/api/consult',(req,res)=>{
+//     ut.ConsultQuestion(req,res)
+// });
+app.post('/api/consult',ut.ConsultQuestion)
+app.get('/api/question/hot', ut.getHotQuestion);
+app.get('/api/question/new', ut.getNewQuestion);
+app.get('/api/question/me/:Username', ut.findUsersQuestions);
